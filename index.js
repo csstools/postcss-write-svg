12a17a456934e1b0d271bc4008cb15f3d90abc11
Jonathan Neal <jonathantneal@hotmail.com>
2015-09-12 13:25:01 -0400

    Update plugin
    Swap iterative methods for inline iteratives
    Cleanup variable names and method names

--- index.js
+++ index.js
@@ -1,56 +1,59 @@
-var extend  = require('util')._extend;
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-write-svg', function (opts) {
-	opts = opts || {};
-
-	var elements = {
-		svg: {
-			xmlns: 'http://www.w3.org/2000/svg'
-		}
-	};
-
-	var svgURI = 'data:image/svg+xml;charset=utf8,';
-
+module.exports = postcss.plugin('postcss-write-svg', function () {
 	function escapeQuotes(string) {
-		return string.replace(/([^\\])"/g, '$1\\"');
+		return string.replace(/(^|[^\\])"/g, '$1\\"');
 	}
 
-	function escapeSVG(string) {
-		return encodeURIComponent(string).replace(/%3C/g, '<').replace(/%3E/g, '>').replace(/%22/g, '"').replace(/%26/g, '%2526').replace(/[()'"]/g, '\\$&');
+	function escapeURI(string) {
+		return encodeURIComponent(string.replace(/&/g, '%26')).replace(/%3C/g, '<').replace(/%3E/g, '>').replace(/%22/g, '"').replace(/[()'"]/g, '\\$&');
 	}
 
-	function createElement(node) {
-		var nodeName   = (node.selector || 'svg').toLowerCase();
-		var markup     = '<' + nodeName;
-		var attributes = extend({}, elements[nodeName]);
-		var children   = [];
+	function escapeContent(string) {
+		return string.replace(/^(['"])(.+)\1$/g, '$2').replace(/</g, '&lt;');
+	}
 
-		if (node.nodes) {
-			node.nodes.forEach(function (childNode) {
+	function createXML(node) {
+		var selector   = node.selector;
+		var tagName    = selector ? selector.toLowerCase() : 'svg';
+		var attributes = selector ? {} : { xmlns: 'http://www.w3.org/2000/svg' };
+		var childNodes = node.nodes;
+		var innerXML   = '';
+		var outerXML   = '<' + tagName;
+
+		if (childNodes) {
+			for (var index = 0, childNode; childNode = childNodes[index]; ++index) {
 				if (childNode.type === 'rule') {
-					children.push(createElement(childNode));
+					innerXML += createXML(childNode);
 				} else if (childNode.type === 'decl') {
 					if (childNode.prop === 'content') {
-						children.push(childNode.value.replace(/^(['"])(.+)\1$/g, '$2').replace(/</g, '&lt;'));
+						innerXML += escapeContent(childNode.value);
 					} else {
 						attributes[childNode.prop] = childNode.value;
 					}
 				}
-			});
-
-			Object.keys(attributes).forEach(function (name) {
-				markup += ' ' + name + '="' + escapeQuotes(attributes[name]) + '"';
-			});
+			}
 		}
 
-		return markup + (children.length ? '>' + children.join('') + '</' + nodeName + '>' : '/>');
+		for (var name in attributes) {
+			outerXML += ' ' + name + '="' + escapeQuotes(attributes[name]) + '"';
+		}
+
+		if (innerXML) {
+			outerXML += '>' + innerXML + '</' + tagName + '>';
+		} else {
+			outerXML += '/>';
+		}
+
+		return outerXML;
 	}
 
 	function addSVG(atrule) {
-		atrule.parent.append(postcss.decl({
+		var value  = 'url("data:image/svg+xml;charset=utf8,' + escapeURI(createXML(atrule)) + '")';
+
+		atrule.parent.insertBefore(atrule, postcss.decl({
 			prop:  'background-image',
-			value: 'url(' + svgURI + escapeSVG(createElement(atrule)) + ')'
+			value: value
 		}));
 
 		atrule.remove();
